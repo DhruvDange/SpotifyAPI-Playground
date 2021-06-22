eb290a8d0df59202d0bb4d8b44d99f58ba948c9f
Dhruv Dange <dhruvdange@gmail.com>
2021-06-22 23:14:56 +0530

    created function to find artist

--- spotify_client.py
+++ spotify_client.py
@@ -3,6 +3,7 @@ import string
 import urllib
 import base64
 import os
+from urllib.parse import quote
 from warnings import resetwarnings
 import requests
 from requests.models import Response
@@ -14,7 +15,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 scope = """ user-read-recently-played, user-top-read, user-read-playback-position,
             app-remote-control, streaming, playlist-read-private, playlist-read-collaborative,
             playlist-modify-public, playlist-modify-private, user-library-modify, 
-            user-library-read """
+            user-library-read"""
 
 
 class SpotifyClient():
@@ -29,17 +30,31 @@ class SpotifyClient():
             cache_handler=None
         )
         self.api_key = spotify_auth.get_access_token()["access_token"]
-        
+
+    
+    def get_id(self, uri):
+        if "artist" in uri:
+            return uri.removeprefix("spotify:artist:")
+        elif "track" in uri:
+            return uri.removeprefix("spotify:track")
+
+
 
     def get_random_tracks(self):
+        url = f'https://api.spotify.com/v1/search'
         wildcard = f'{random.choice(string.ascii_lowercase)}'
         query = urllib.parse.quote(wildcard)
         offset = random.randint(1,2000)
-
-        url = f'https://api.spotify.com/v1/search?q={query}&offset={offset}&type=track'
-
+        payload = {
+            "q": query,
+            "offset": offset,
+            "market": "US",
+            "type": "track",
+            "limit": 2
+        }
         response = requests.get(
             url,
+            params = payload,
             headers={
                 "Content-Type": "application/json",
                 "Authorization": f"Bearer {self.api_key}"
@@ -48,6 +63,7 @@ class SpotifyClient():
         response_json = response.json()
         tracks = [tracks for tracks in response_json['tracks']['items']]
         print(f"Found {len(tracks)} tracks.")
+        print(response_json)
         return tracks
 
 
@@ -68,16 +84,24 @@ class SpotifyClient():
 
 
     def get_popular_tracks(self, popularity):
-        market = "US"
+        url = f"https://api.spotify.com/v1/search"
         pop_tracks = []
 
         while(len(pop_tracks)<=20):
+            
             wildcard = f'{random.choice(string.ascii_lowercase)}'
             query = urllib.parse.quote(wildcard)
             offset = random.randint(0,1000)
-            url = f"https://api.spotify.com/v1/search?q={query}&offset={offset}&market={market}&type=track&limit=20"
+            payload = {
+                "q": query,
+                "offset": offset,
+                "market": "US",
+                "type": "track",
+                "limit": 20
+            }
             response = requests.get(
                 url,
+                params = payload,
                 headers={
                     "Authorization": f"Bearer {self.api_key}",
                     "Content_type": "application/json"
@@ -89,4 +113,40 @@ class SpotifyClient():
                 if (track["popularity"] >= popularity):
                     pop_tracks.append(track)
         return pop_tracks
+
+    
+    def get_artist_uri(self, artist_name):
+        query = artist_name
+        item_type = "artist"
+        url = "https://api.spotify.com/v1/search"
+        payload = {
+            "q": query,
+            "type": item_type
+        }
+        header = {
+            "Content-type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+        response = requests.get(url, params = payload, headers = header)
+        response_json = response.json()
+        artists = [artist for artist in response_json['artists']['items']]
+
+        keys = range(1, len(artists)+1)
+        values = []
+        for artist in artists:
+            name = artist["name"]
+            followers = artist["followers"]["total"]
+            values.append(f"{name}, followers: {followers}")
+        artist_names = dict(zip(keys, values))
+        for key, value in artist_names.items():
+            print(f"{key}: {value}")
+        print("Enter number corresponding to artist: ")
+        artist_index = int(input("")) - 1
+        selected_artist = artists[artist_index]["uri"]
+        
+        return(self.get_id(selected_artist))
+
+
+
 
--- test_code.py
+++ test_code.py
@@ -21,3 +21,15 @@
         # print(self.api_key)
 
 # x = SpotifyClient(os.getenv("SPOTIFY_CLIENT_ID"), os.getenv("SPOTIFY_CLIENT_SECRET"))
+
+# import webbrowser
+# # webbrowser.open('http://127.0.0.1:8000', new=2)
+
+# import requests
+# import urllib.request
+# url = "https://accounts.spotify.com/authorize?client_id=f5de6d932e734c108d504646c0e30f88&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000&scope=user-read-private%20user-read-email&state=34fFs29kd09"
+
+# response = urllib.request.urlopen("https://accounts.spotify.com/authorize?client_id=f5de6d932e734c108d504646c0e30f88&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000&scope=user-read-private%20user-read-email&state=34fFs29kd09")
+
+#  # https://stackoverflow.com/questions/27554994/spotify-api-authentication-with-python
+# webbrowser.open_new_tab(url)
